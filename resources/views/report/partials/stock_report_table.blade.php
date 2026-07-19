f2ff300a602933d85f01cfa7e3ea843d88b81de0
towfiqueemrose <towfiqueemrose@gmail.com>
2026-07-19 22:50:01 +0600

    refactor: restyle stock report table with Tailwind CSS classes for modern UI

--- resources/views/report/partials/stock_report_table.blade.php
+++ resources/views/report/partials/stock_report_table.blade.php
@@ -5,50 +5,66 @@
   $product_custom_field3 = !empty($custom_labels['product']['custom_field_3']) ? $custom_labels['product']['custom_field_3'] : __('lang_v1.product_custom_field3');
   $product_custom_field4 = !empty($custom_labels['product']['custom_field_4']) ? $custom_labels['product']['custom_field_4'] : __('lang_v1.product_custom_field4');
 @endphp
-<table class="table table-bordered table-striped" id="stock_report_table">
-    <thead>
+<table class="table tw-w-full tw-text-sm tw-text-left tw-text-gray-600 tw-whitespace-nowrap tw-border-collapse custom-modern-table" id="stock_report_table">
+    <thead class="tw-bg-gray-50 tw-text-gray-700 tw-uppercase tw-text-xs tw-border-b tw-border-gray-200">
         <tr>
-            <th>@lang('messages.action')</th>
-            <th>SKU</th>
-            <th>@lang('business.product')</th>
-            <th>@lang('lang_v1.variation')</th>
-            <th>@lang('product.category')</th>
-            <th>@lang('sale.location')</th>
-            <th>@lang('purchase.unit_selling_price')</th>
-            <th>@lang('report.current_stock')</th>
+            <th class="tw-px-4 tw-py-3">@lang('messages.action')</th>
+            <th class="tw-px-4 tw-py-3">SKU</th>
+            <th class="tw-px-4 tw-py-3">@lang('business.product')</th>
+            <th class="tw-px-4 tw-py-3">@lang('lang_v1.variation')</th>
+            <th class="tw-px-4 tw-py-3">@lang('product.category')</th>
+            <th class="tw-px-4 tw-py-3">@lang('sale.location')</th>
+            <th class="tw-px-4 tw-py-3">@lang('purchase.unit_selling_price')</th>
+            <th class="tw-px-4 tw-py-3">@lang('report.current_stock')</th>
             @can('view_product_stock_value')
-            <th class="stock_price">@lang('lang_v1.total_stock_price') <br><small>(@lang('lang_v1.by_purchase_price'))</small></th>
-            <th>@lang('lang_v1.total_stock_price') <br><small>(@lang('lang_v1.by_sale_price'))</small></th>
-            <th>@lang('lang_v1.potential_profit')</th>
+            <th class="stock_price tw-px-4 tw-py-3">@lang('lang_v1.total_stock_price') <small class="tw-text-gray-500">(@lang('lang_v1.by_purchase_price'))</small></th>
+            <th class="tw-px-4 tw-py-3">@lang('lang_v1.total_stock_price') <small class="tw-text-gray-500">(@lang('lang_v1.by_sale_price'))</small></th>
+            <th class="tw-px-4 tw-py-3">@lang('lang_v1.potential_profit')</th>
             @endcan
-            <th>@lang('report.total_unit_sold')</th>
-            <th>@lang('lang_v1.total_unit_transfered')</th>
-            <th>@lang('lang_v1.total_unit_adjusted')</th>
-            <th>{{$product_custom_field1}}</th>
-            <th>{{$product_custom_field2}}</th>
-            <th>{{$product_custom_field3}}</th>
-            <th>{{$product_custom_field4}}</th>
+            <th class="tw-px-4 tw-py-3">@lang('report.total_unit_sold')</th>
+            <th class="tw-px-4 tw-py-3">@lang('lang_v1.total_unit_transfered')</th>
+            <th class="tw-px-4 tw-py-3">@lang('lang_v1.total_unit_adjusted')</th>
+            <th class="tw-px-4 tw-py-3">{{$product_custom_field1}}</th>
+            <th class="tw-px-4 tw-py-3">{{$product_custom_field2}}</th>
+            <th class="tw-px-4 tw-py-3">{{$product_custom_field3}}</th>
+            <th class="tw-px-4 tw-py-3">{{$product_custom_field4}}</th>
             @if($show_manufacturing_data)
-                <th class="current_stock_mfg">@lang('manufacturing::lang.current_stock_mfg') @show_tooltip(__('manufacturing::lang.mfg_stock_tooltip'))</th>
+                <th class="current_stock_mfg tw-px-4 tw-py-3">@lang('manufacturing::lang.current_stock_mfg') @show_tooltip(__('manufacturing::lang.mfg_stock_tooltip'))</th>
             @endif
         </tr>
     </thead>
     <tfoot>
-        <tr class="bg-gray font-17 text-center footer-total">
-            <td colspan="7"><strong>@lang('sale.total'):</strong></td>
-            <td class="footer_total_stock"></td>
+        <tr class="tw-bg-gray-100 tw-font-bold tw-text-gray-900 tw-text-center footer-total tw-border-t tw-border-gray-200">
+            <td colspan="7" class="tw-px-4 tw-py-4"><strong>@lang('sale.total'):</strong></td>
+            <td class="footer_total_stock tw-px-4 tw-py-4"></td>
             @can('view_product_stock_value')
-            <td class="footer_total_stock_price"></td>
-            <td class="footer_stock_value_by_sale_price"></td>
-            <td class="footer_potential_profit"></td>
+            <td class="footer_total_stock_price tw-px-4 tw-py-4"></td>
+            <td class="footer_stock_value_by_sale_price tw-px-4 tw-py-4"></td>
+            <td class="footer_potential_profit tw-px-4 tw-py-4"></td>
             @endcan
-            <td class="footer_total_sold"></td>
-            <td class="footer_total_transfered"></td>
-            <td class="footer_total_adjusted"></td>
-            <td colspan="4"></td>
+            <td class="footer_total_sold tw-px-4 tw-py-4"></td>
+            <td class="footer_total_transfered tw-px-4 tw-py-4"></td>
+            <td class="footer_total_adjusted tw-px-4 tw-py-4"></td>
+            <td colspan="4" class="tw-px-4 tw-py-4"></td>
             @if($show_manufacturing_data)
-                <td class="footer_total_mfg_stock"></td>
+                <td class="footer_total_mfg_stock tw-px-4 tw-py-4"></td>
             @endif
         </tr>
     </tfoot>
 </table>
+<style>
+    #stock_report_table {
+        white-space: nowrap !important;
+    }
+    #stock_report_table th, #stock_report_table td {
+        white-space: nowrap !important;
+        width: auto !important;
+    }
+    .custom-modern-table tbody tr:hover {
+        background-color: #f9fafb !important;
+    }
+    .custom-modern-table tbody td {
+        padding: 0.75rem 1rem !important;
+        border-bottom: 1px solid #e5e7eb !important;
+    }
+</style>
